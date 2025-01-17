5ee1196e477a1e3caf8ebebb473e1ad5d9bf0227
Papa-HHaven <114277306+Papa-HHaven@users.noreply.github.com>
2025-01-17 18:55:58 +0800

    resume changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/test/',  // This should match the name of your GitHub repository
+  base: '/MarblsLats/',  // This should match the name of your GitHub repository
   server: {
     proxy: {
       '/submit': 'https://api.web3forms.com',  // Proxy to Web3Forms API
