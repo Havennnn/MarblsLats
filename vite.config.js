e7535d85853ee795f72957d7206e613fdafa5318
Papa-HHaven <114277306+Papa-HHaven@users.noreply.github.com>
2025-01-27 20:09:46 +0800

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MarblsLats/',  // This should match the name of your GitHub repository
+  base: '/MarblsLats/', // Adjust as necessary for your deployment path
   server: {
     proxy: {
-      '/submit': 'https://api.web3forms.com',  // Proxy to Web3Forms API
+      '/submit': 'https://api.web3forms.com', // Proxy to Web3Forms API
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // All vendor modules will go into a separate chunk
+          }
+        },
+      },
     },
   },
 })
