1c4200d065aba84b9ce8a234ad0f0784694cc1d9
Papa-HHaven <114277306+Papa-HHaven@users.noreply.github.com>
2025-01-27 21:14:18 +0800

    refined preloader

--- src/component/Preloader.jsx
+++ src/component/Preloader.jsx
@@ -5,9 +5,9 @@ const Preloader = () => {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-dgray z-50">
       <div className="animate-spin rounded-full border-b-4 border-white border-solid h-24 w-24 lg:h-32 lg:w-32 flex justify-center items-center">
-      <div className="animate-spin rounded-full border-t-4 border-white border-solid h-16 w-16 lg:h-24 lg:w-24"></div>
+      <div className="animate-spin rounded-full border-t-4 border-white border-solid h-[72px] w-[72px] lg:h-24 lg:w-24"></div>
       </div>
-      <img src={logo} alt="Loading" className="absolute h-8 w-8 lg:h-12 lg:w-12" />
+      <img src={logo} alt="Loading" className="absolute h-10 w-10 lg:h-12 lg:w-12" />
     </div>
   );
 };
