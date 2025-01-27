582ca300ebf17419c82a0ceb87561814f18161c6
Papa-HHaven <114277306+Papa-HHaven@users.noreply.github.com>
2025-01-27 20:58:22 +0800

    vite update again

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,25 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MarblsLats/', // Adjust as necessary for your deployment path
-  server: {
-    proxy: {
-      '/submit': 'https://api.web3forms.com', // Proxy to Web3Forms API
-    },
-  },
+  base: '/MarblsLats/',
+
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor'; // All vendor modules will go into a separate chunk
+            return 'vendor';
           }
         },
       },
+    },
+    assetsInclude: ['**/*.jsx', '**/*.js'],
+    chunkSizeWarningLimit: 1000,
+  },
+
+  server: {
+    proxy: {
+      '/submit': 'https://api.web3forms.com',
     },
   },
 })
